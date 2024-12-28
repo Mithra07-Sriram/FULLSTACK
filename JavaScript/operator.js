39dd6ce275cd56a5cd1a1198690b7cc05751fccb
Mithra07-Sriram <mithrasriram07@gmail.com>
2024-12-28 11:24:19 +0530

    addingoperators

--- JavaScript/operator.js
+++ JavaScript/operator.js
@@ -12,7 +12,33 @@ console.log(x);
 */
 //uranary operator
 
-a=10;
+/*a=10;
 console.log(a++);
 console.log(a);
+*/
 
+//relational operator
+
+/*let a=10;
+let b="10";
+if(a==b)
+{
+    console.log("a is equal to b");
+}
+else{
+    console.log("a is not equal to b");
+}
+ //prints a is equal 
+//== operator see only the value not the datatype
+//=== operator see both the value and datatype
+*/
+let a=10;
+let b="10";
+if(a===b)
+{
+    console.log("a is equal to b");
+}
+else{
+    console.log("a is not equal to b");
+}
+//prints a is not equal to b
