565194dd81c0e9dfc45d49bbea10c1d016710d8f
Mithra07-Sriram <mithrasriram07@gmail.com>
2024-12-30 16:22:53 +0530

    Adding day 4 work

--- fullstackPlacement/src/App.jsx
+++ fullstackPlacement/src/App.jsx
@@ -1,18 +1,30 @@
-import './App.css'
-import ClassCompEg from './Components/ClassComponents/ClassCompEg';
-import DefaultExplanation from './Components/FunctionalComponents/DefaultExplanation';
-import UnorderedList from './Components/FunctionalComponents/UnorderedList';
+import './App.css';
+import ClassCompEg from "./Components/ClassComponents/ClassCompEg";
+import Home from "./Components/FunctionalComponents/Home";
+import About from "./Components/FunctionalComponents/About";
+import Contact from "./Components/FunctionalComponents/Contact";
+import Gallery from "./Components/FunctionalComponents/Gallery";
+import NavBar from "./Components/FunctionalComponents/NavBar";
+import "./assets/css/Home.css";
+import "./assets/css/NavBar.css";
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+
 
 function App() {
- 
-
   return (
     <div>
-      <ClassCompEg />
-      <DefaultExplanation />
-      <UnorderedList />
-      </div>
-  )
+      <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={ <About college="Kongu" clg1="naturopathy" />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      </BrowserRouter>
+
+    </div>
+  );
 }
 
 export default App;
